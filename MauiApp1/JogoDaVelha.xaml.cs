368b9ebdaaa896fec9f53cff6afd77d115d8e6ed
Alex <prof.alex.etec.2021@gmail.com>
2024-10-21 14:52:04 -0300

    Jogo da velha, feio mas completo

--- MauiApp1/JogoDaVelha.xaml.cs
+++ MauiApp1/JogoDaVelha.xaml.cs
@@ -3,14 +3,15 @@ namespace MauiApp1;
 public partial class JogoDaVelha : ContentPage
 {
     string vez = "X";
-	public JogoDaVelha()
-	{
-		InitializeComponent();
-	}
+    public JogoDaVelha()
+    {
+        InitializeComponent();
+        ((Label)((Grid)Content).Children[0]).Text = $"Jogo da Velha - {vez}";
+    }
 
     private void btnVoltar_Clicked(object sender, EventArgs e)
     {
-		Navigation.PopAsync();
+        Navigation.PopAsync();
     }
 
     private void Button_Clicked(object sender, EventArgs e)
@@ -21,51 +22,108 @@ public partial class JogoDaVelha : ContentPage
         botao.Text = vez;
 
         if (!Vitoria("X"))
-        {
             if (!Vitoria("O"))
-            {
-                if (!btn10.IsEnabled &&
-                   !btn11.IsEnabled &&
-                   !btn12.IsEnabled)
-                {
-                    DisplayAlert("Empate", "Deu Velha!", "Jogar novamente");
-
-                    Zerar();
-                }
-            }
-        }
+                Velha();
 
         vez = vez.Equals("X") ? "O" : "X";
+        ((Label)((Grid)Content).Children[0]).Text = $"Jogo da Velha - {vez}";
+    }
+
+    private async void Velha()
+    {
+        foreach (var item in (Grid)this.Content)
+        {
+            if (item is Button)
+            {
+                var btn = (Button)item;
+                if (btn.Text != "Voltar")
+                    if (btn.IsEnabled)
+                        return;
+            }
+        }
+        await DisplayAlert("Empate", "Deu Velha!", "Jogar novamente");
+
+        Zerar();
     }
 
     private bool Vitoria(string jogador)
     {
-        //1ª condição de vitória X
-        if (btn10.Text == jogador && btn11.Text == jogador && btn12.Text == jogador)
-        {
-            DisplayAlert("Parabéns", $"Vitória do Jogador {jogador}", "Jogar novamente");
+        int linha, coluna;
 
-            Zerar();
+        //Linhas
+        for (linha = 1; linha < ((Grid)Content).Children.Count - 1; linha += 3)
+        {
+            for (coluna = 0; coluna <= 2; coluna++)
+                if (GetButton(linha + coluna).Text != jogador)
+                    break;
+
+            if (coluna > 2)
+            {
+                ExibirMensagem(jogador);
+                return true;
+            }
+        }
+
+        //Colunas
+        for (coluna = 0; coluna <= 2; coluna++)
+        {
+            for (linha = 1; linha < ((Grid)Content).Children.Count - 1; linha += 3)
+                if (GetButton(linha + coluna).Text != jogador)
+                    break;
+
+            if (linha == 10)
+            {
+                ExibirMensagem(jogador);
+                return true;
+            }
+        }
+        //Diagonal \
+        if (GetButton(1).Text == jogador &&
+            GetButton(5).Text == jogador &&
+            GetButton(9).Text == jogador)
+        {
+            ExibirMensagem(jogador);
             return true;
         }
 
-
-
-
-
+        //Diagonal /
+        if (GetButton(3).Text == jogador &&
+            GetButton(5).Text == jogador &&
+            GetButton(7).Text == jogador)
+        {
+            ExibirMensagem(jogador);
+            return true;
+        }
 
         return false;
-        
+    }
+
+    private Button GetButton(int index)
+    {
+        return (Button)((Grid)Content).Children[index];
+    }
+
+    private async void ExibirMensagem(string jogador)
+    {
+        await DisplayAlert("Parabéns", $"Vitória do Jogador {jogador}", "Jogar novamente");
+
+        Zerar();
     }
 
     private void Zerar()
     {
-        btn10.Text = "";
-        btn11.Text = String.Empty;
-        btn12.Text = "";
+        foreach (var item in (Grid)this.Content)
+        {
+            if (item is Button)
+            {
+                var btn = (Button)item;
 
-        btn10.IsEnabled = true;
-        btn11.IsEnabled = true;
-        btn12.IsEnabled = true;
+                if (btn.Text != "Voltar")
+                {
+                    btn.IsEnabled = true;
+                    btn.Text = "";
+                }
+            }
+        }
     }
 }
